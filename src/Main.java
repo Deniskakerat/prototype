9920c500fa7b39dfaa55fc6c536e28cd3820b5f8
denslatv <denslats@gmail.com>
2022-10-05 18:18:59 +0300

    Prototype

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        ShapeCashe.loadCache();
 
+        Shape clonedShape = (Shape)ShapeCashe.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCashe.getShape ("2");
+        System.out.println ("Shape "+clonedShape2.getType ());
+
+        Shape clonedShape3 = (Shape) ShapeCashe.getShape( "3");
+        System.out.println ("Shape : " +clonedShape3.getType());
+
+        Shape clonedShape4 = (Shape) ShapeCashe.getShape("2");
+        System.out.println("Shape " + clonedShape4.getType());
     }
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,9 @@
-public class Rectangle {
+public class Rectangle  extends Shape {
+    public Rectangle(){
+        type = "Rectangle";
+    }
+    @Override
+    public void draw(){
+        System.out.println("Inside Rectangle::draw() method");
+    }
 }
